24dff8019d0b6e9091d11a24e3a638d4fc7bb3da
KantaiMishima <mishimakantai@gmail.com>
2025-11-08 10:31:08 +0900

    feat: formとして値を送信できるように対応

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useGetPetById } from "../api/pet/pet";
+import { useGetPetById, useUpdatePet } from "../api/pet/pet";
 import type { Pet, PetStatus } from "../api/model";
 import {
   Avatar,
@@ -9,28 +9,53 @@ import {
   MenuItem,
   Select,
   TextField,
+  Typography,
 } from "@mui/material";
+import { Field, Form, type FormRenderProps } from "react-final-form";
+import { useCallback } from "react";
+import type { Config } from "final-form";
+import axios from "axios";
 
 const PetForm = ({
-  values
-}: { values: Pet }) => {
+  values,
+  handleSubmit,
+  invalid,
+  dirty,
+}: FormRenderProps<Pet>) => {
   return (
     <Grid container direction="column" alignItems="start" spacing={4}>
       <Grid>
-        <Avatar src={values.photoUrls?.[0]} alt={values.name} />
+        <Avatar src={values?.photoUrls?.[0]} alt={values?.name} />
       </Grid>
       <Grid>
-        <TextField
-            value={values.name}
-            label="名前"
-        />
+        <Field name="name">
+          {({ input, meta }) => (
+            <TextField
+              {...input}
+              label="名前"
+              error={Boolean(meta.error)}
+              helperText={meta.error}
+            />
+          )}
+        </Field>
       </Grid>
       <Grid>
-        <Select value={values.status} label="ステータス">
-          <MenuItem value={"available" as PetStatus}>購入可能</MenuItem>
-          <MenuItem value={"pending" as PetStatus}>入荷待ち</MenuItem>
-          <MenuItem value={"sold" as PetStatus}>売り切れ</MenuItem>
-        </Select>
+        <Field name="status">
+          {({ input, meta }) => (
+            <>
+              <Select {...input} label="ステータス" error={Boolean(meta.error)}>
+                <MenuItem value={"available" as PetStatus}>購入可能</MenuItem>
+                <MenuItem value={"pending" as PetStatus}>入荷待ち</MenuItem>
+                <MenuItem value={"sold" as PetStatus}>売り切れ</MenuItem>
+              </Select>
+              {meta.error && (
+                <Typography style={{ color: "red", marginTop: "0.5rem" }}>
+                  {meta.error}
+                </Typography>
+              )}
+            </>
+          )}
+        </Field>
       </Grid>
       <Grid container spacing={2}>
         <Grid>
@@ -43,6 +68,8 @@ const PetForm = ({
             type="submit"
             variant="contained"
             color="primary"
+            onClick={handleSubmit}
+            disabled={invalid || !dirty}
           >
             保存
           </Button>
@@ -58,6 +85,26 @@ export const Detail: React.FC<{
   const state = useGetPetById(petId, {
     swr: { revalidateOnFocus: false },
   });
+  const mutation = useUpdatePet();
+
+  const updatePet: Config<Pet>["onSubmit"] = useCallback(
+    async (values, formApi, _callback) => {
+      try {
+        const { data } = await mutation.trigger(values);
+        formApi.reset(data);
+      } catch (e) {
+        if (axios.isAxiosError(e)) {
+          // 雑にエラー表示
+          window.alert(e.message);
+        } else {
+          window.alert(
+            "予期せぬエラーが発生しました。時間をおいて再実行してください。"
+          );
+        }
+      }
+    },
+    [mutation.trigger]
+  );
 
   if (state.isLoading) {
     return <>loading</>;
@@ -69,5 +116,5 @@ export const Detail: React.FC<{
     return <>データが存在しません</>;
   }
   const pet = state.data.data;
-  return <PetForm values={pet} />;
+  return <Form initialValues={pet} onSubmit={updatePet} render={PetForm} />;
 };
