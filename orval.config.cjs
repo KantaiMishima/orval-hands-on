188592b715cefb052ec2f47b59bec01c3cf0ff6e
KantaiMishima <mishimakantai@gmail.com>
2025-11-08 10:32:21 +0900

    chore: orvalの設定にzodを追加

--- orval.config.cjs
+++ orval.config.cjs
@@ -1,13 +1,25 @@
 module.exports = {
- "petstore-file": {
-   input: "./docs/openapi.yml",
-   output: {
-     mode: "tags-split",
-     target: "src/api/petstore.ts",
-     baseUrl: "https://petstore3.swagger.io/api/v3",
-     schemas: "./src/api/model",
-     mock: true,
-     client: "swr",
-   },
- },
+  "petstore-file": {
+    input: "./docs/openapi.yml",
+    output: {
+      mode: "tags-split",
+      target: "src/api/petstore.ts",
+      baseUrl: "https://petstore3.swagger.io/api/v3",
+      schemas: "./src/api/model",
+      mock: true,
+      client: "swr",
+    },
+  },
+  // 追記
+  "petstore-zod": {
+    input: {
+      target: "./docs/openapi.yml",
+    },
+    output: {
+      mode: 'tags-split',
+      client: 'zod',
+      target: 'src/api/petstore.zod.ts',
+      fileExtension: '.zod.ts',
+    },
+  },
 };
