7584b0aa970119e1b99acab4baf2fb1ab1dfdc90
KantaiMishima <mishimakantai@gmail.com>
2025-11-08 10:28:10 +0900

    feat: form要素を実装

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,5 +1,56 @@
 import React from "react";
 import { useGetPetById } from "../api/pet/pet";
+import type { Pet, PetStatus } from "../api/model";
+import {
+  Avatar,
+  Button,
+  Grid,
+  Link,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+
+const PetForm = ({
+  values
+}: { values: Pet }) => {
+  return (
+    <Grid container direction="column" alignItems="start" spacing={4}>
+      <Grid>
+        <Avatar src={values.photoUrls?.[0]} alt={values.name} />
+      </Grid>
+      <Grid>
+        <TextField
+            value={values.name}
+            label="名前"
+        />
+      </Grid>
+      <Grid>
+        <Select value={values.status} label="ステータス">
+          <MenuItem value={"available" as PetStatus}>購入可能</MenuItem>
+          <MenuItem value={"pending" as PetStatus}>入荷待ち</MenuItem>
+          <MenuItem value={"sold" as PetStatus}>売り切れ</MenuItem>
+        </Select>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid>
+          <Button variant="outlined" LinkComponent={Link} href="/">
+            キャンセル
+          </Button>
+        </Grid>
+        <Grid>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            保存
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 export const Detail: React.FC<{
   petId: number;
@@ -18,5 +69,5 @@ export const Detail: React.FC<{
     return <>データが存在しません</>;
   }
   const pet = state.data.data;
-  return <div>{pet.name}</div>;
+  return <PetForm values={pet} />;
 };
